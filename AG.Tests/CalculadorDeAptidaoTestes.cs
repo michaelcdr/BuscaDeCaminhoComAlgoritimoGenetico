e306bb4030c4c8186dd39fac9d80391fd32314dd
Michael <michaelcdr@hotmail.com>
2020-07-13 21:03:47 -0300

    unit tests modified 2

--- AG.Tests/CalculadorDeAptidaoTestes.cs
+++ AG.Tests/CalculadorDeAptidaoTestes.cs
@@ -6,37 +6,37 @@ namespace AG.Tests
     public class CalculadorDeAptidaoTestes
     {
         [Fact]
-        public void TestarAptidaoComSaidaDoCenario()
+        public void TestarValorDeAptidaoComSaidaDoCenario()
         {
             DetalhamentoCalculoDeAptidao detalhamentoCalculoDeAptidao = CalculadorDeAptidao.Calcular("001111111111");
 
             int valorDeAptidadoObtida = detalhamentoCalculoDeAptidao.Aptidao;
 
-            int valorDeAptidadoEsperada = 100;
+            int valorDeAptidadoEsperada = 100 + (Parametros.PontosPorCelulaOcupada * 6);
 
             Assert.Equal(valorDeAptidadoEsperada, valorDeAptidadoObtida);
         }
 
         [Fact]
-        public void TestarAptidaoComChegadaAteOFim()
+        public void TesteDeValorDeAptidaoComChegadaAteOFimDoLabirinto()
         {
             DetalhamentoCalculoDeAptidao detalhamentoCalculoDeAptidao = CalculadorDeAptidao.Calcular("000001010100");
             
             int valorDeAptidadoObtida = detalhamentoCalculoDeAptidao.Aptidao;
 
-            int valorDeAptidadoEsperada = 0;
+            int valorDeAptidadoEsperada = (Parametros.PontosPorCelulaOcupada * 6);
 
             Assert.Equal(valorDeAptidadoEsperada, valorDeAptidadoObtida);
         }
 
         [Fact]
-        public void TestarAptidaoComQuandoOcorreColisaoEmParedes()
+        public void TesteDeValorDeAptidaoQuandoOcorrerDuasColisoesEmParedes()
         {
             DetalhamentoCalculoDeAptidao detalhamentoCalculoDeAptidao = CalculadorDeAptidao.Calcular("000101010000");
 
             int valorDeAptidadoObtida = detalhamentoCalculoDeAptidao.Aptidao;
 
-            int valorDeAptidadoEsperada = 20;
+            int valorDeAptidadoEsperada = (Parametros.PontosPorAtravessiaDeParedes * 2) + (Parametros.PontosPorCelulaOcupada * 6);
 
             Assert.Equal(valorDeAptidadoEsperada, valorDeAptidadoObtida);
         }
